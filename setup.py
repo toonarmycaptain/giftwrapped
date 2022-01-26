f366f85b955180b29655aacfd8d54179a0e9dccb
toonarmycaptain <toonarmycaptain@hotmail.com>
2022-01-25 22:17:37 -0600

    Change package name for PyPI upload.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="giftwrapped-toonarmycaptain",
+    name="giftwrapped",
     version="0.0.1",
     author="David Antonini",
     author_email="toonarmycaptain@hotmail.com",
